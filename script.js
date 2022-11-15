429282dd71ae0d98b8b82d6684e2865cc62ec95a
Emanuel Joness <itzemanuelj@gmail.com>
2022-11-15 00:09:38 -0500

    es6 and es7 if statement pratice

---
+++ script.js
@@ -0,0 +1,19 @@
+// Code an if statement that tests whether pCode is "10010". If so, assign to the variable city the string "New York". city has already been declared.
+
+if (pCode === '10010') {
+    city = 'NYC';
+    console.log();
+}
+let pCode = '10010' ? city = 'nyc' : city != 'NYC';
+
+// Code an if statement. Test whether a variable has a particular numerical value. If so, assign a new value to that variable, as in x = 1;
+if (cost === 2) {
+    x = 1;
+}
+let cost = 2 ? x = 1 : x != 1;
+
+// Code the first line of an if statement that tests whether a variable has the value of a particular string.
+if (fname === 'Justin') {
+    fname != 'justn'
+}
+let fname = 'Justin' ? fname = 'Justin' : fname != 'justin';
